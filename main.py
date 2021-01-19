9a65a53a35fecf009d25183945bf5055d52dd51f
Tudor <tudordascalu1@gmail.com>
2021-01-19 23:01:49 +0100

    Logs train vs test error

--- main.py
+++ main.py
@@ -34,15 +34,23 @@ if not has_arguments or "1" in arguments:
     Train neural network and save model to file
     """
     # Init model
-    nn = neural_net.Neural_net(n_dim=1, n_neurons=20, learning_rate=1e-2)
+    nn = neural_net.Neural_net(n_dim=1, n_neurons=20, learning_rate=1e-1)
     # Fit model
-    error_train, error_val = nn.fit(X_train, y_train, batch_size=5, threshold_gradient=1e-20, threshold_bad=10, n_epochs=50000, X_val=X_val, y_val=y_val, verbose=True)
+    error_train, error_val = nn.fit(X_train, y_train, batch_size=10, threshold_gradient=1e-20, threshold_bad=10, n_epochs=1000, X_val=X_val, y_val=y_val, verbose=True)
     # Make predictions
     y_pred = nn.predict(X_val)
     # Plot results
-    plt.scatter(X_val[:, 0], y_pred, label="Prediction")
-    plt.scatter(X_val[:, 0], y_val, label="True")
-    plt.legend()
+    fig, ax = plt.subplots(nrows=1, ncols=2)
+    ax[0].plot(np.log(error_train), c="b", label="Train")
+    ax[0].plot(np.log(error_val), c="r", label="Val")
+    ax[0].set_title("Train vs Val error")
+    ax[0].set_ylabel("Error (logscale)")
+    ax[0].set_xlabel("Iteration")
+    ax[0].legend()
+    ax[1].scatter(X_val[:, 0], y_pred, label="Prediction")
+    ax[1].scatter(X_val[:, 0], y_val, label="True")
+    ax[1].set_title("Predicted labels vs true labels")
+    ax[1].legend()
     plt.show()
 
 if "2" in arguments:
